94958f4aba82b911aa8d469d979bd7dcb8d12b17
milon mac <milonsheikh88@gamil.com>
2021-07-13 23:40:04 +0600

    upload flutter_loading_more_refresh

---
+++ flutter_loading_more_refresh/android/app/src/main/java/com/milon/flutter_loading_refresh/MainActivity.java
@@ -0,0 +1,6 @@
+package com.milon.flutter_loading_refresh;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutter_loading_more_refresh/lib/post.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'models.dart';
+
+class Post extends StatelessWidget {
+final PostModel post;
+
+ Post({Key key, @required this.post,})  : assert(post != null), super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      // leading: Image.network(
+      //   post.avatar,
+      //   width: 60.0,
+      //   height: 60.0,
+      // ),
+      title: Text(post.body),
+    );
+  }
+}
