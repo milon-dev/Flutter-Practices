59b47325df4c76071a425d6f8c6c690bdd678926
milon mac <milonsheikh88@gamil.com>
2021-09-05 00:56:17 +0600

    upload flutter_autocomplete_validator

---
+++ flutter_autocomplete_validator/android/app/src/main/java/com/milonsheikh/flutter_autocomplete_validator/MainActivity.java
@@ -0,0 +1,6 @@
+package com.milonsheikh.flutter_autocomplete_validator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutter_autocomplete_validator/lib/main.dart
@@ -0,0 +1,171 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:substring_highlight/substring_highlight.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Autocomplete validator',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _formKey = GlobalKey<FormState>();
+  late TextEditingController controller;
+
+  final List<String> autoCompleteData = [
+    'Afghanistan',
+    'Albania',
+    'Algeria',
+    'Andorra',
+    'Bahamas',
+    'Bahrain',
+    'Bangladesh',
+    'Barbados',
+    'Cambodia',
+    'Cameroon',
+    'Canada',
+    'Chad'
+  ];
+
+  void validateAndSave() {
+    if (_formKey.currentState!.validate()) {
+      print('=======Form is valid========');
+      String textValue = controller.text.toString();
+      print("textValue ========>>> $textValue");
+    } else {
+      print('Form is invalid');
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Autocomplete validator"),
+      ),
+      body:Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Autocomplete(
+                optionsBuilder: (TextEditingValue textEditingValue) {
+                  if (textEditingValue.text.isEmpty) {
+                    return const Iterable<String>.empty();
+                  } else {
+                    return autoCompleteData.where((word) =>
+                        word.toLowerCase()
+                            .startsWith(textEditingValue.text.toLowerCase()));
+                  }
+                },
+                optionsViewBuilder:(context, Function(String) onSelected, options) {
+                  return Align(
+                    alignment: Alignment.topLeft,
+                    child: Material(
+                      elevation: 4,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width - 80,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Color(0xff9D50BB).withOpacity(0.8),
+                                Color(0xff6E48AA).withOpacity(0.9),
+                              ],
+                            )),
+                        child: ListView.separated(
+                          shrinkWrap: true,
+                          padding: EdgeInsets.zero,
+                          itemCount: options.length,
+                          itemBuilder: (context, index) {
+                            final option = options.elementAt(index);
+                            return GestureDetector(
+                              onTap: () {
+                                onSelected(option.toString());
+                              },
+                              child: Container(
+                                padding: EdgeInsets.fromLTRB(5, 10.0, 5, 10.0),
+                                child: SubstringHighlight(
+                                  textAlign: TextAlign.left,
+                                  text: option.toString(),
+                                  term: controller.text,
+                                  textStyleHighlight: TextStyle(fontWeight: FontWeight.w700),
+                                ),
+                              ),
+                            );
+                          },
+                          // The separators
+                          separatorBuilder: (context, index) {
+                            return Divider(
+                              color:Colors.white,
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                onSelected: (selectedString) {
+                  print(selectedString);
+                },
+                fieldViewBuilder: (context, controller, focusNode, onEditingComplete) {
+                  this.controller = controller;
+                  return TextFormField(
+                    controller: controller,
+                    focusNode: focusNode,
+                    onEditingComplete: onEditingComplete,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter country';
+                      }
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.grey[300]!),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.grey[500]!),
+                      ),
+                      hintText: "Country name",
+                    ),
+
+                  );
+                },
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              RaisedButton(
+                child: Text('Submit'),
+                onPressed: validateAndSave,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
